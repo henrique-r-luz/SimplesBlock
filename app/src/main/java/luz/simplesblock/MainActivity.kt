c651cb72aafe3223cd3baf544e24d7b5c3fb0d35
henrique_r_luz <henrique_r_luz@yahoo.com.br>
2024-02-04 23:35:52 -0300

    mostra tela de acessibilidade apenas uma vez.

--- app/src/main/java/luz/simplesblock/MainActivity.kt
+++ app/src/main/java/luz/simplesblock/MainActivity.kt
@@ -4,19 +4,33 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
+import android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS
 import androidx.annotation.RequiresApi
-import luz.simplesblock.model.PopulaApp
 import luz.simplesblock.service.AppBlockService
 
 class MainActivity : AppCompatActivity() {
+    private val sharedPreferencesKey = "showServiceSettings"
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startAcessibikit()
+        // Verificar se a tela ServiceSettingsActivity já foi mostrada
+        val sharedPreferences = getPreferences(MODE_PRIVATE)
+        val showServiceSettings = sharedPreferences.getBoolean(sharedPreferencesKey, true)
+
+        if (showServiceSettings) {
+
+            // Se ainda não foi mostrada, abrir a tela e salvar o estado
+            startActivity(Intent(ACTION_ACCESSIBILITY_SETTINGS))
+            //val intent = Intent(this, ServiceSettingsActivity::class.java)
+            //startActivity(intent)
+
+            // Salvar o estado para que a tela não seja mostrada novamente
+            val editor = sharedPreferences.edit()
+            editor.putBoolean(sharedPreferencesKey, false)
+            editor.apply()
+        }
 
     }
 
@@ -27,7 +41,4 @@ class MainActivity : AppCompatActivity() {
         startForegroundService(serviceIntent)
     }
 
-    private fun startAcessibikit(){
-        startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
-    }
 }
